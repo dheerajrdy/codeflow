bcebbb80219108baadc0037a32137bc488571b22
Dheeraj Reddy <dheerajbreddy@gmail.com>
2025-12-04 13:49:05 -0800

    Delete docs/design/reference/mas-main directory

--- docs/design/reference/mas-main/picoagents/src/picoagents/webui/frontend/src/components/message_renderer/index.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Message renderer exports
- */
-
-export { MessageRenderer } from "./MessageRenderer";
-export { ContentRenderer } from "./ContentRenderer";
-export type { MessageRendererProps, RenderProps } from "./types";
